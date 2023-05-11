94d6e12227b67ea35ba737f711ea021d6ea711cd
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-05-11 23:37:50 +0900

    Update featuresにもTailwindが効く用に修正

--- src/features/SideMenu.tsx
+++ src/features/SideMenu.tsx
@@ -13,19 +13,23 @@ export const SideMenu: FC<Props> = ({
   setSelectedItem,
 }) => {
   return (
-    <div className="p-4 bg-black text-white">
+    <div className="h-full px-4 py-8 bg-gray-800 text-white">
       <ul>
         {list &&
           list.map((item) => (
             <li
               key={item.id}
-              onClick={() => setSelectedItem(item)}
               className={clsx(
                 item !== selectedItem && "opacity-100",
-                item === selectedItem && "opacity-50"
+                item === selectedItem && "opacity-50 pointer-events-none"
               )}
             >
-              {item.title}
+              <button
+                className="text-left w-full p-2 hover:opacity-80"
+                onClick={() => setSelectedItem(item)}
+              >
+                {item.title}
+              </button>
             </li>
           ))}
       </ul>
