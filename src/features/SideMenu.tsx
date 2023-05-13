3c118008013700b37e5820c0075e4b2eca14b06e
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-05-13 20:23:16 +0900

    Update 追加ボタンを調整

--- src/features/SideMenu.tsx
+++ src/features/SideMenu.tsx
@@ -1,15 +1,16 @@
 import { FC, useState } from "react";
-import clsx from "clsx";
 import { useList } from "./useList";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export const SideMenu: FC = () => {
   const { list, addItem } = useList();
   const [selectedItem, setSelectedItem] = useState<Item>();
   return (
     <div className="h-full px-4 py-8 bg-gray-800 text-white">
-      <ul>
-        {list &&
-          list.map((item) => (
+      {list && list.length > 0 && (
+        <ul className="mb-8">
+          {list.map((item) => (
             <li
               key={item.id}
               className={clsx(
@@ -18,19 +19,20 @@ export const SideMenu: FC = () => {
               )}
             >
               <button
-                className="text-left w-full p-2 hover:opacity-80"
+                className="text-left w-full py-2 hover:opacity-80"
                 onClick={() => setSelectedItem(item)}
               >
                 {item.title}
               </button>
             </li>
           ))}
-      </ul>
+        </ul>
+      )}
       <button
-        className="bg-white text-gray-800 w-full py-2 rounded-tr-md rounded-bl-md"
+        className="bg-white text-gray-800 w-full py-2 rounded-tr-md rounded-bl-md transition-opacity hover:opacity-80"
         onClick={addItem}
       >
-        add
+        <PlusIcon className="w-[20px] m-auto" />
       </button>
     </div>
   );
