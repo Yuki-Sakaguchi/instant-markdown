ae37c8dc91adc406bfce8348b41ca4692de5f8ff
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-05-14 22:20:53 +0900

    Update 更新タイミングの調整

--- src/features/Editor.tsx
+++ src/features/Editor.tsx
@@ -1,10 +1,11 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import { useList } from "./useList";
 import Image from "next/image";
 import { TrashIcon } from "@heroicons/react/24/outline";
+import { format } from "date-fns";
 
 const AboutView: FC = () => {
   return (
@@ -33,11 +34,41 @@ const AboutView: FC = () => {
   );
 };
 
+function useComposing() {
+  const [isComposing, setIsComposing] = useState(false);
+
+  // IME入力中かどうかを判定する処理
+  useEffect(() => {
+    window.addEventListener("compositionstart", startHandler);
+    window.addEventListener("compositionend", endHander);
+    return () => {
+      window.removeEventListener("compositionstart", startHandler);
+      window.removeEventListener("compositionend", endHander);
+    };
+  }, []);
+
+  function startHandler() {
+    setIsComposing(true);
+  }
+
+  function endHander() {
+    setIsComposing(false);
+  }
+
+  return {
+    isComposing,
+    setIsComposing,
+  };
+}
+
 /**
  * エディターコンポーネント
  */
 export const Tiptap: FC = () => {
   const { selectedItem, onChange, removeItem } = useList();
+  const { isComposing } = useComposing();
+  const [updateTimer, setUpdateTimer] = useState<NodeJS.Timeout>();
+  const [cursorPoint, setCursorPoint] = useState(0);
 
   const editor = useEditor({
     extensions: [
@@ -46,35 +77,71 @@ export const Tiptap: FC = () => {
         placeholder: "メモを書きましょう！",
       }),
     ],
-    onUpdate(data) {
-      onChange(data.editor.getHTML());
-    },
+    onUpdate,
   });
 
-  useEffect(() => {
-    if (editor == null || selectedItem == null) return;
-    editor.commands.setContent(selectedItem.body);
-    editor.commands.focus();
-  }, [selectedItem, editor]);
+  /**
+   * データの更新処理
+   *
+   * 更新するたびに実行しないように setTimeout を用いて一番最後の update 時のみに動くように調整
+   * IME入力中の場合にも変更を無視する
+   * また、更新時のカーソルの位置も保存して、データ更新後に使う
+   */
+  function onUpdate() {
+    if (editor == null) return;
+    if (updateTimer) clearTimeout(updateTimer);
+    setUpdateTimer(
+      setTimeout(() => {
+        if (isComposing) return;
+        setCursorPoint(editor?.state.selection.anchor);
+        onChange(editor.getHTML());
+      }, 400)
+    );
+  }
 
-  const deleteItem = () => {
+  function deleteItem() {
     if (selectedItem == null) return;
     if (confirm("記事を削除してもよろしいでしょうか？")) {
       removeItem(selectedItem.id);
     }
-  };
+  }
+
+  function formatDate(date: Date) {
+    return format(new Date(date), "yyyy-MM-dd HH:mm:ss");
+  }
+
+  // IME判定後もアップデート処理を実行
+  useEffect(() => {
+    onUpdate();
+  }, [isComposing]);
+
+  // データが更新されたら反映する
+  useEffect(() => {
+    if (editor == null || selectedItem == null) return;
+    editor.commands.setContent(selectedItem.body);
+    editor.commands.focus(cursorPoint);
+    if (!editor.isFocused) editor.commands.focus();
+  }, [selectedItem, editor]);
 
   return (
-    <div className="h-full pl-4 py-4">
+    <div className="h-full pl-4">
       {selectedItem == null ? (
         <div className="h-full flex justify-center items-center">
           <AboutView />
         </div>
       ) : (
-        <div className="relative">
-          <EditorContent className="h-full" editor={editor} />
+        <div className="overflow-scroll relative h-full flex flex-col py-4">
+          <div className="mb-4">
+            <time className="block text-xs text-gray-400">
+              作成日 : {formatDate(selectedItem.createdAt)}
+            </time>
+            <time className="block text-xs text-gray-400">
+              更新日 : {formatDate(selectedItem.updatedAt)}
+            </time>
+          </div>
+          <EditorContent className="flex-1" editor={editor} />
           <button
-            className="absolute -top-2 right-2 p-2 transition-opacity opacity-50 hover:opacity-100"
+            className="absolute top-3 right-3 p-2 transition-opacity opacity-50 hover:opacity-100"
             onClick={deleteItem}
           >
             <TrashIcon className="w-[20px]" />
